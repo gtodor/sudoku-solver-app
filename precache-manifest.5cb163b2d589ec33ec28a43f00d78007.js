e682c133384d95dd10a58ed8ba8656a54073ee97
Andrei Todor <bvandrei94@gmail.com>
2019-09-14 01:28:59 +0200

    Updates

---
+++ precache-manifest.5cb163b2d589ec33ec28a43f00d78007.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cccf2f41f2a134b042411fb8eda3c7d4",
+    "url": "/sudoku-solver-app/index.html"
+  },
+  {
+    "revision": "ed6945f9a9c1d59d606b",
+    "url": "/sudoku-solver-app/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "9128bbe88cdee2214b2b",
+    "url": "/sudoku-solver-app/static/css/main.46593b78.chunk.css"
+  },
+  {
+    "revision": "ed6945f9a9c1d59d606b",
+    "url": "/sudoku-solver-app/static/js/2.f1004c1d.chunk.js"
+  },
+  {
+    "revision": "9128bbe88cdee2214b2b",
+    "url": "/sudoku-solver-app/static/js/main.763de177.chunk.js"
+  },
+  {
+    "revision": "0d709bd1edae43345ecc",
+    "url": "/sudoku-solver-app/static/js/runtime~main.31e9f83e.js"
+  }
+]);
